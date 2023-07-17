6d78748d4d1ebba6c52ab1888266b47f0ae6ff35
Vinícius Souza <tradutorvinicius@gmail.com>
2023-07-17 15:37:23 -0300

    Resolve requisito 2

---
+++ src/models/User.js
@@ -0,0 +1,36 @@
+const UserModel = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    displayName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      field: 'display_name',
+    },
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    image: {
+      allowNull: true,
+      type: DataTypes.STRING,
+    },
+  }, {
+    timestamps: false,
+    tableName:'users',
+    underscored: true
+  });
+
+
+  return User;
+}
+
+module.exports = UserModel;
